1a19362be53a9e56ce03ef6243ee065dad9e3a06
Eric Dennis <eric.dennis@sleepygiant.com>
2015-06-17 13:37:28 -0700

    Add hive 1.2.0 formula

---
+++ hive120.rb
@@ -0,0 +1,28 @@
+class Hive120 < Formula
+  desc "Hadoop-based data summarization, query, and analysis"
+  homepage 'https://hive.apache.org'
+  url 'https://www.apache.org/dyn/closer.cgi?path=hive/hive-1.2.0/apache-hive-1.2.0-bin.tar.gz'
+  sha256 "38569761590b68e11aab950cffb8af39fd11bc923e65ad7415aa0b080796ef0d"
+
+  depends_on 'hadoop'
+  depends_on :java
+
+  def install
+    rm_f Dir["bin/ext/*.cmd", "bin/ext/util/*.cmd"]
+    libexec.install %w[bin conf examples hcatalog lib scripts]
+    bin.write_exec_script Dir["#{libexec}/bin/*"]
+  end
+
+  def caveats; <<-EOS.undent
+    Hadoop must be in your path for hive executable to work.
+    After installation, set $HIVE_HOME in your profile:
+      export HIVE_HOME=#{libexec}
+
+    If you want to use HCatalog with Pig, set $HCAT_HOME in your profile:
+      export HCAT_HOME=#{libexec}/hcatalog
+
+    You may need to set JAVA_HOME:
+      export JAVA_HOME="$(/usr/libexec/java_home)"
+    EOS
+  end
+end
